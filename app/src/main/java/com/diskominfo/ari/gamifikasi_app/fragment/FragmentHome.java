b8e39c2c4115122861c542b612393c67d7b11352
arikurniawans <ari.curnia.one@gmail.com>
2019-07-01 21:10:02 +0700

    Placepicker

--- app/src/main/java/com/diskominfo/ari/gamifikasi_app/fragment/FragmentHome.java
+++ app/src/main/java/com/diskominfo/ari/gamifikasi_app/fragment/FragmentHome.java
@@ -1,14 +1,22 @@
 package com.diskominfo.ari.gamifikasi_app.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,12 +26,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.diskominfo.ari.gamifikasi_app.R;
 
+import static android.app.Activity.RESULT_OK;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentHome extends Fragment{
-
+private int PLACE_PICKER_REQUEST = 1;;
+Button btnPeta;
     public FragmentHome() {
         // Required empty public constructor
     }
@@ -35,9 +46,41 @@ public class FragmentHome extends Fragment{
 
         final View view = inflater.inflate(R.layout.fragment_beranda, container, false);
 
+        btnPeta = view.findViewById(R.id.btnPeta);
+
+        btnPeta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    //menjalankan place picker
+                    startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
+
+                    // check apabila <a title="Solusi Tidak Bisa Download Google Play Services di Android" href="http://www.twoh.co/2014/11/solusi-tidak-bisa-download-google-play-services-di-android/" target="_blank">Google Play Services tidak terinstall</a> di HP
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         return  view;
     }
 
 
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = (Place) PlacePicker.getPlace(data, getActivity());
+                String toastMsg = String.format(
+                        "Place: %s \n" +
+                                "Alamat: %s \n" +
+                                "Latlng %s \n", place.getName(), place.getAddress(), place.getLatLng().latitude+" "+place.getLatLng().longitude);
+                Toast.makeText(getActivity(),"Data "+toastMsg,Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
